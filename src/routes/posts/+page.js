ef08140b92e0af46bb99bb2cd1cf4da301037b19
Sebastian Lammers <sebastian.lammers@posteo.de>
2022-12-16 09:18:51 +0100

    why?

--- src/routes/posts/+page.js
+++ src/routes/posts/+page.js
@@ -1,7 +1,11 @@
 export async function load({ fetch }) {
+  console.log("running fetch now")
   const res = await fetch('/api/posts.json')
   const allPosts = await res.json()
 
+  console.log("res is: ", res);
+  console.log("------------------------");
+  console.log("allPosts is: ", allPosts);
   return {
     posts: allPosts
   }
