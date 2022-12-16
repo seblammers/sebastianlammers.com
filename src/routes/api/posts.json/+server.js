3a28e3531ec2e06efbe31a160beca753207c5933
Sebastian Lammers <sebastian.lammers@posteo.de>
2022-12-16 09:12:35 +0100

    huh

--- src/routes/api/posts.json/+server.js
+++ src/routes/api/posts.json/+server.js
@@ -1,6 +1,6 @@
 import { json } from '@sveltejs/kit'
 
-export const GET = async () => {
+export async function GET() {
     const allPostFiles = import.meta.glob('../../posts/**/*.md')
     const iterablePostFiles = Object.entries(allPostFiles)
   
