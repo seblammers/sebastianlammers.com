7c03b367e03891311dae52f071db507f8516a319
Sebastian Lammers <sebastian.lammers@posteo.de>
2022-12-18 22:16:27 +0100

    add more verbose error

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -8,3 +8,9 @@
 
 <h1>{$page.status}</h1>
 <h2>{$page.error.message}</h2>
+
+<p><strong>Sorry!</strong> Do you want to go to:</p>
+<ul>
+	<li><a href="/">Home</a></li>
+	<li><a href="/posts">Posts</a></li>
+</ul>
