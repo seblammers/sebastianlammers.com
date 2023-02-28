4038cc3d5c73a50c3171318e6c2204a8bde3cbe4
Sebastian Lammers <sebastian.lammers@posteo.de>
2023-02-28 21:20:01 +0100

    init simple wave divider

---
+++ src/lib/components/svg/WaveDivider.svelte
@@ -0,0 +1,20 @@
+<svg
+	class="curve"
+	preserveAspectRatio="none"
+	viewBox="0 0 1440 120"
+	fill="none"
+	xmlns="http://www.w3.org/2000/svg"
+	><path
+		d="m0 96 80-5.3C160 85 320 75 480 64s320-21 480-21.3c160 .3 320 10.3 400 16l80 5.3V0H0v96Z"
+	/></svg
+>
+
+<style>
+	.curve {
+		fill: var(--paper);
+		background-color: var(--ink);
+		display: block;
+		height: 3.5em;
+		width: 100%;
+	}
+</style>
