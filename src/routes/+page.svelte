8285440ad19620160c07f9d53891736aefb1694b
Sebastian Lammers <sebastian.lammers@posteo.de>
2023-04-26 13:36:33 +0200

    add transitions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,8 @@
 <script>
 	import self from '$lib/assets/images/cv/profile_removebg_BW.png';
-	import Heading from '$lib/components/Heading.svelte';
 	import Image from '$lib/components/Image.svelte';
-
 	import { fly } from 'svelte/transition';
+
 	let width;
 	$: mobile = width < 900;
 </script>
@@ -12,15 +11,15 @@
 <div class="flex-container flow" class:mobile>
 	<!-- <img src={self} alt="Portrait of Sebastian Lammers" height="500" width="500" /> -->
 	<div class="flex-items">
-		<Image src={self} alt="Portrait of Sebastian Lammers" />
+		<Image src={self} alt="Portrait of Sebastian Lammers" loading="eager" />
 	</div>
 
-	<div class="flex-items intro" in:fly={{ x: 50, duration: 850, delay: 200 }}>
+	<div class="flex-items intro" in:fly={{ x: 50, duration: 850, delay: 300 }}>
 		<h3 id="hi">Hi, I'm Sebastian</h3>
 		<p>and I'm a researcher with a passion for <strong>data visualization</strong>.</p>
 	</div>
 
-	<div class="flex-items more" in:fly={{ x: 50, duration: 850, delay: 1050 }}>
+	<div class="flex-items more" in:fly={{ x: 50, duration: 850, delay: 1500 }}>
 		<p>
 			I love <strong>working with data</strong> and <strong>visualizing it</strong> effectively. And
 			I <strong>love to help others</strong> do the same.
