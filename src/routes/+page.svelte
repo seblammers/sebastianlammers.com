5dd4cb3c599170411b0e8df7057585d4228dd1fa
Sebastian Lammers <sebastian.lammers@posteo.de>
2023-04-26 12:12:08 +0200

    init landing page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,109 @@
+<script>
+	import self from '$lib/assets/images/cv/profile_removebg_BW.png';
+	import Heading from '$lib/components/Heading.svelte';
+	import Image from '$lib/components/Image.svelte';
+
+	import { fly } from 'svelte/transition';
+	let width;
+	$: mobile = width < 900;
+</script>
+
+<svelte:window bind:innerWidth={width} />
+<div class="flex-container flow" class:mobile>
+	<!-- <img src={self} alt="Portrait of Sebastian Lammers" height="500" width="500" /> -->
+	<div class="flex-items">
+		<Image src={self} alt="Portrait of Sebastian Lammers" />
+	</div>
+
+	<div class="flex-items intro" in:fly={{ x: 50, duration: 850, delay: 200 }}>
+		<h3 id="hi">Hi, I'm Sebastian</h3>
+		<p>and I'm a researcher with a passion for <strong>data visualization</strong>.</p>
+	</div>
+
+	<div class="flex-items more" in:fly={{ x: 50, duration: 850, delay: 1050 }}>
+		<p>
+			I love <strong>working with data</strong> and <strong>visualizing it</strong> effectively. And
+			I <strong>love to help others</strong> do the same.
+		</p>
+		<br />
+
+		<p>
+			This website is <strong>my playground</strong> to learn about HTML, CSS & JavaScript.
+			Sometimes I write
+			<a sveltekit:prefetch href="/posts">blog posts</a>
+			on things I learned (to help others learn it too) and share
+			<a sveltekit:prefetch href="/projects">projects</a> that I have built.
+		</p>
+	</div>
+</div>
+
+<style lang="scss">
+	.flex-container {
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+		justify-content: flex-start;
+		align-items: flex-end;
+		align-content: flex-start;
+	}
+
+	.flex-items:nth-child(1) {
+		display: block;
+		flex-grow: 0;
+		flex-shrink: 0;
+		flex-basis: 50%;
+		align-self: auto;
+		order: 0;
+	}
+
+	.flex-items:nth-child(2) {
+		display: block;
+		flex-grow: 0;
+		flex-shrink: 0;
+		flex-basis: 50%;
+		align-self: auto;
+		order: 0;
+	}
+
+	.flex-items:nth-child(3) {
+		display: block;
+		flex-grow: 0;
+		flex-shrink: 0;
+		flex-basis: 99%;
+		align-self: flex-start;
+		order: 0;
+	}
+
+	.intro {
+		margin-bottom: 1rem;
+		margin-top: 0%;
+	}
+	.mobile {
+		display: flex;
+		flex-direction: column;
+		flex-wrap: nowrap;
+		justify-content: center;
+		align-items: center;
+		align-content: center;
+		--flow-space: var(--space-xs);
+
+		& > .flex-items {
+			max-width: 80vw;
+			flex-shrink: 0;
+			align-self: flex-start;
+			padding-left: var(--space-s);
+		}
+	}
+
+	.intro > h3 {
+		&:hover {
+			--_p: 0%; // override hoverstyles
+			cursor: default;
+		}
+	}
+
+	#hi {
+		font-weight: 400 !important;
+		font-size: var(--step-2);
+	}
+</style>
